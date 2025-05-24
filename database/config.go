da8d1b2a95ead628d27fd5924621995451f7299b
Muhammad Afif <muhammadafif@192.168.1.2>
2025-05-24 13:41:45 +0700

    dotenv

--- database/config.go
+++ database/config.go
@@ -2,22 +2,36 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectDB(){
-	var err error
-	const MYSQL = "root:root@tcp(127.0.0.1:8889)/fibergo?charset=utf8mb4&parseTime=True&loc=Local"
+func ConnectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	DSN := MYSQL
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
 
-	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, host, port, name,
+	)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Can't connect to database")
 	}
+
 	fmt.Println("Connected to database")
 }
